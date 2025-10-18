99e7c6b47066dead270d6277fca55bb94d5ac9a8
wubmu <923185028@qq.com>
2025-10-18 16:18:36 +0800

    first commit

---
+++ react-projects/01-hello-world/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>Hello World!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+    </>
+  )
+}
+
+export default App
